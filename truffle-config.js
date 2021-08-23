cbfb9c11ba3f4488aa2c31882e064b0081ceb712
Lim Chee Kin <limcheekin@vobject.com>
2021-08-23 10:44:27 +0800

    chore: updated config and installed @truffle/hdwallet-provider to deploy contract to rinkeby network

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -72,6 +72,20 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+        // Useful for deploying to a public network.
+    // It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          `https://rinkeby.infura.io/v3/b874a2f145f84dc5a8466e5490816511`
+        ),
+      network_id: 4, // Rinkeby's id
+      gas: 5500000,
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
