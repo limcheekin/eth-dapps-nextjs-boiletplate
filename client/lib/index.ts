57b5420eae01e0163173e882db5a595de1dd5520
Lim Chee Kin <limcheekin@vobject.com>
2021-08-26 10:34:52 +0800

    feat: restructured code to remove duplicates

--- client/lib/index.ts
+++ client/lib/index.ts
@@ -2,16 +2,15 @@ import Web3 from 'web3'
 import detectEthereumProvider from '@metamask/detect-provider'
 import { Dispatch } from 'react'
 import { ActionType } from '../store/types'
-
-import WalletConnectProvider from "@walletconnect/web3-provider"
-import { JsonRpcPayload, JsonRpcResponse } from 'web3-core-helpers';
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { JsonRpcPayload, JsonRpcResponse } from 'web3-core-helpers'
 import { AbstractProvider } from 'web3-core/types'
 
 export declare class WalletConnectWeb3Provider extends WalletConnectProvider implements AbstractProvider {
   sendAsync(payload: JsonRpcPayload, callback: (error: Error | null, result?: JsonRpcResponse) => void): void;
 }
 
-export async function handleInjectedProvider(dispatch: Dispatch<ActionType>)  {
+export async function handleInjectedProvider(dispatch: Dispatch<ActionType>) {
   const injectedProvider: any = await detectEthereumProvider()
   let provider: any
 
@@ -21,19 +20,21 @@ export async function handleInjectedProvider(dispatch: Dispatch<ActionType>)  {
   } else {
     console.log(`No web3 instance injected, using Local web3.`)
     provider = new Web3.providers.HttpProvider('http://localhost:7545')
-  }  
+  }
   const web3 = new Web3(provider)
   // REF: https://docs.metamask.io/guide/getting-started.html#connecting-to-metamask
   const accounts = await provider.request({ method: 'eth_requestAccounts' });
-  // const accounts = await web3.eth.getAccounts()
-  console.log('accounts', accounts, 'isMetaMask', provider.isMetaMask)
-  dispatch({ type: 'SET_ACCOUNT', payload: accounts[0] })
-  dispatch({ type: 'SET_PROVIDER', payload: provider })
-  dispatch({ type: 'SET_WEB3', payload: web3 })
-  return { account: accounts[0], web3 }
+  
+  // The following code seems not working if trigger disconnect from MetaMask Chrome extension
+  provider.on("disconnect", (error: any) => {
+    dispatch({ type: 'CLEAR_STATE' })
+    console.log(error)
+  });
+  
+  return dispatchStates(dispatch, accounts[0], provider, web3)
 }
 
-export async function handleWalletConnect(dispatch: Dispatch<ActionType>)  {
+export async function handleWalletConnect(dispatch: Dispatch<ActionType>) {
   //  Create WalletConnect Provider
   const provider = new WalletConnectProvider({
     infuraId: "b874a2f145f84dc5a8466e5490816511",
@@ -42,17 +43,22 @@ export async function handleWalletConnect(dispatch: Dispatch<ActionType>)  {
   // Enable session (triggers QR Code modal)
   await provider.enable()
 
+  const web3 = new Web3(provider as WalletConnectWeb3Provider)
+  const accounts = await web3.eth.getAccounts()
+
   provider.on("disconnect", (code: number, reason: string) => {
-    dispatch({ type: 'CLEAR_STATE'})
+    dispatch({ type: 'CLEAR_STATE' })
     console.log(code, reason)
   });
 
-  const web3 = new Web3(provider as WalletConnectWeb3Provider)
-  const accounts = await web3.eth.getAccounts()
-  console.log('accounts', accounts, 'isMetaMask', provider.isMetaMask)
-  dispatch({ type: 'SET_ACCOUNT', payload: accounts[0] })
+  return dispatchStates(dispatch, accounts[0], provider, web3)
+}
+
+function dispatchStates(dispatch: Dispatch<ActionType>, account: string, provider: any, web3: Web3) {
+  console.log('account', account, 'isMetaMask', provider.isMetaMask)
+  dispatch({ type: 'SET_ACCOUNT', payload: account })
   dispatch({ type: 'SET_PROVIDER', payload: provider })
   dispatch({ type: 'SET_WEB3', payload: web3 })
-  return { account: accounts[0], web3 }
+  return { account, web3 }
 }
 
